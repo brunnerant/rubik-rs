9298399e8a75d60316858d7920220c4b5ebe89c1
Antoine Brunner <brunnerant@gmail.com>
2026-05-10 21:33:11 +0200

    More consistent move order

--- rubik-lib/src/moves.rs
+++ rubik-lib/src/moves.rs
@@ -2,18 +2,18 @@ use derive_more::derive::Display;
 
 #[derive(Debug, Display)]
 pub enum Face {
-    #[display("F")]
-    Front,
-    #[display("B")]
-    Back,
     #[display("L")]
     Left,
     #[display("R")]
     Right,
-    #[display("U")]
-    Up,
     #[display("D")]
     Down,
+    #[display("U")]
+    Up,
+    #[display("B")]
+    Back,
+    #[display("F")]
+    Front,
 }
 
 #[derive(Debug)]
@@ -41,30 +41,6 @@ pub struct Move {
 }
 
 impl Move {
-    pub const F: Move = Move {
-        face: Face::Front,
-        dir: Direction::Clockwise,
-    };
-    pub const F_: Move = Move {
-        face: Face::Front,
-        dir: Direction::CounterClockwise,
-    };
-    pub const F2: Move = Move {
-        face: Face::Front,
-        dir: Direction::HalfTurn,
-    };
-    pub const B: Move = Move {
-        face: Face::Back,
-        dir: Direction::Clockwise,
-    };
-    pub const B_: Move = Move {
-        face: Face::Back,
-        dir: Direction::CounterClockwise,
-    };
-    pub const B2: Move = Move {
-        face: Face::Back,
-        dir: Direction::HalfTurn,
-    };
     pub const L: Move = Move {
         face: Face::Left,
         dir: Direction::Clockwise,
@@ -89,18 +65,6 @@ impl Move {
         face: Face::Right,
         dir: Direction::HalfTurn,
     };
-    pub const U: Move = Move {
-        face: Face::Up,
-        dir: Direction::Clockwise,
-    };
-    pub const U_: Move = Move {
-        face: Face::Up,
-        dir: Direction::CounterClockwise,
-    };
-    pub const U2: Move = Move {
-        face: Face::Up,
-        dir: Direction::HalfTurn,
-    };
     pub const D: Move = Move {
         face: Face::Down,
         dir: Direction::Clockwise,
@@ -113,21 +77,57 @@ impl Move {
         face: Face::Down,
         dir: Direction::HalfTurn,
     };
+    pub const U: Move = Move {
+        face: Face::Up,
+        dir: Direction::Clockwise,
+    };
+    pub const U_: Move = Move {
+        face: Face::Up,
+        dir: Direction::CounterClockwise,
+    };
+    pub const U2: Move = Move {
+        face: Face::Up,
+        dir: Direction::HalfTurn,
+    };
+    pub const B: Move = Move {
+        face: Face::Back,
+        dir: Direction::Clockwise,
+    };
+    pub const B_: Move = Move {
+        face: Face::Back,
+        dir: Direction::CounterClockwise,
+    };
+    pub const B2: Move = Move {
+        face: Face::Back,
+        dir: Direction::HalfTurn,
+    };
+    pub const F: Move = Move {
+        face: Face::Front,
+        dir: Direction::Clockwise,
+    };
+    pub const F_: Move = Move {
+        face: Face::Front,
+        dir: Direction::CounterClockwise,
+    };
+    pub const F2: Move = Move {
+        face: Face::Front,
+        dir: Direction::HalfTurn,
+    };
 }
 
 #[cfg(test)]
 mod tests {
     use crate::moves::Move;
-
+    
     #[test]
     fn move_display() {
+        assert_eq!(Move::L.to_string(), "L");
+        assert_eq!(Move::R.to_string(), "R");
+        assert_eq!(Move::D.to_string(), "D");
+        assert_eq!(Move::U.to_string(), "U");
+        assert_eq!(Move::B.to_string(), "B");
         assert_eq!(Move::F.to_string(), "F");
         assert_eq!(Move::F_.to_string(), "F'");
         assert_eq!(Move::F2.to_string(), "F2");
-        assert_eq!(Move::B.to_string(), "B");
-        assert_eq!(Move::L.to_string(), "L");
-        assert_eq!(Move::R.to_string(), "R");
-        assert_eq!(Move::U.to_string(), "U");
-        assert_eq!(Move::D.to_string(), "D");
     }
 }
