4433ed91c85505b4f293a1a2dcbfae8d9269d5b5
Antoine Brunner <brunnerant@gmail.com>
2026-05-20 23:22:14 +0200

    Implement state ordering

--- rubik-lib/src/trie.rs
+++ rubik-lib/src/trie.rs
@@ -25,14 +25,14 @@ impl Trie {
         let mut node = self;
         let mut branches = [(0, 0); 2 * 8 + 2 * 12];
         let mut b = 0;
-        for i in (0..8).rev() {
+        for i in 0..8 {
             let pos = State::get_block(state.corners, 5 * i + 2, 3);
             let ori = State::get_block(state.corners, 5 * i, 2);
             branches[b] = (pos as usize, 8);
             branches[b + 1] = (ori as usize, 3);
             b += 2;
         }
-        for i in (0..12).rev() {
+        for i in 0..12 {
             let pos = State::get_block(state.edges, 5 * i + 1, 4);
             let ori = State::get_block(state.edges, 5 * i, 1);
             branches[b] = (pos as usize, 12);
@@ -113,11 +113,32 @@ impl Trie {
     }
 }
 
+impl std::cmp::PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl std::cmp::Ord for State {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        fn rev(bits: u64, n: usize) -> u64 {
+            let mut res = 0;
+            for i in 0..n {
+                res |= ((bits >> (5 * i)) & 0b11111) << (5 * (n - i - 1))
+            }
+            res
+        }
+
+        (rev(self.corners, 8), rev(self.edges, 12))
+            .cmp(&(rev(other.corners, 8), rev(other.edges, 12)))
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::{moves::Move, util::Moves};
 
-use super::*;
+    use super::*;
     #[test]
     fn test_trie_insert_and_iter() {
         let mut trie = Trie::new();
@@ -128,14 +149,14 @@ use super::*;
         trie.insert(state2);
         trie.insert(state3);
         let states: Vec<State> = trie.iter().collect();
-        assert_eq!(states, vec![state3, state2, state1]);
+        assert_eq!(states, vec![state1, state2, state3]);
     }
 
     #[test]
     fn test_trie_ordering() {
         let mut trie = Trie::new();
         let mut states: Vec<_> = Moves::to_depth(4).iter().map(|(_, s)| s).collect();
-        states.sort_by_key(|s| (s.corners, s.edges));
+        states.sort();
         for &state in states.iter() {
             trie.insert(state);
         }
