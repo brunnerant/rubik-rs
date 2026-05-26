f5810f6464933d87bf81e8e83a6a197608eb40a1
Antoine Brunner <brunnerant@gmail.com>
2026-05-26 23:54:22 +0200

    More space-efficient trie

--- rubik-lib/examples/trie_stats.rs
+++ rubik-lib/examples/trie_stats.rs
@@ -1,15 +1,13 @@
-use rubik_lib::{trie::Trie, util::Moves};
+use rubik_lib::{trie::TrieBuilder, util::Moves};
 
 fn main() {
-    let mut trie = Trie::new();
+    let mut trie = TrieBuilder::new();
     let mut count = 0;
     for (_, state) in Moves::to_depth(5).iter() {
         count += 1;
         trie.insert(state);
     }
-    let stats = trie.stats();
-    println!("number of states:   {}", count);
-    println!("avg branching:      {:.2}", stats.avg_branching());
-    println!("avg segment length: {:.1}", stats.avg_segment_length());
-    println!("memory use:         {} MB", trie.footprint() / 1024 / 1024);
+    let trie = trie.build();
+    println!("number of states: {}", count);
+    println!("memory use:       {} MB", trie.footprint() / 1024 / 1024);
 }
--- rubik-lib/src/trie.rs
+++ rubik-lib/src/trie.rs
@@ -1,50 +1,32 @@
-
 use itertools::Itertools;
 
 use crate::state::State;
 
-pub struct Trie {
+pub struct TrieBuilder {
     branches: Vec<usize>,
     states: Vec<State>,
 }
 
-#[derive(Default)]
-pub struct Stats {
-    pub num_nodes: usize,
-    pub max_num_nodes: usize,
-    pub num_segments: usize,
+// branch structure:
+// - 6 bits give the depth in the uncompressed trie
+// - 4 bits give the index of the node with respect to its possible siblings
+// - 4 bits for each possible child
+//   - if 0, no child
+//   - if >0, points to relative u64 that points to child
+pub struct Trie {
+    branches: Vec<u64>,
+    states: Vec<State>,
 }
 
-impl Stats {
-    pub fn avg_branching(&self) -> f64 {
-        self.num_nodes as f64 / self.max_num_nodes as f64
-    }
-
-    pub fn avg_segment_length(&self) -> f64 {
-        self.num_nodes as f64 / self.num_segments as f64
-    }
-}
-
-impl std::ops::Add for Stats {
-    type Output = Stats;
-
-    fn add(self, rhs: Self) -> Self::Output {
-        Stats {
-            num_nodes: self.num_nodes + rhs.num_nodes,
-            max_num_nodes: self.max_num_nodes + rhs.max_num_nodes,
-            num_segments: self.num_segments + rhs.num_segments,
-        }
-    }
-}
-
-impl Default for Trie {
+impl Default for TrieBuilder {
     fn default() -> Self {
-        Trie::new()
+        TrieBuilder::new()
     }
 }
 
-impl Trie {
+impl TrieBuilder {
     const NO_CHILD: usize = usize::MAX;
+    const MAX_DEPTH: u64 = 2 * 8 + 2 * 12;
 
     pub fn new() -> Self {
         Self {
@@ -90,101 +72,167 @@ impl Trie {
         self.states.push(state);
     }
 
-    fn is_leaf(s: usize) -> bool {
-        s >= 2 * 8 + 2 * 12
+    pub fn build(self) -> Trie {
+        let mut branches = Vec::new();
+        self.build_branch(0, 0, 0, &mut branches);
+        Trie {
+            branches,
+            states: self.states,
+        }
     }
 
-    fn max_children(s: usize) -> usize {
-        match (s < 2 * 8, s % 2 == 0) {
+    fn build_branch(
+        &self,
+        node: usize,
+        d: u64,
+        sibling_idx: u64,
+        branches: &mut Vec<u64>,
+    ) -> usize {
+        let max_children = Self::max_children(d);
+        let num_children = self.branches[node..node + max_children]
+            .iter()
+            .filter(|&&n| n != Self::NO_CHILD)
+            .count();
+        let header_offset = branches.len();
+        branches.append(&mut vec![0; num_children + 1]);
+        let mut child_idx = 0;
+        let mut header = (d << 58) | (sibling_idx << 54);
+        for (idx, &child) in self.branches[node..node + max_children]
+            .iter()
+            .enumerate()
+            .filter(|&(_, &n)| n != Self::NO_CHILD)
+        {
+            let (mut child, child_d, sibling_idx) = self.segment_end(child, d + 1, idx as u64);
+            if Self::is_leaf(child_d) {
+                child |= 1 << 63;
+            } else {
+                child = self.build_branch(child, child_d, sibling_idx, branches);
+            }
+
+            child_idx += 1;
+            header |= (child_idx as u64) << (4 * idx);
+            branches[header_offset + child_idx] = child as u64;
+        }
+        branches[header_offset] = header;
+        header_offset
+    }
+
+    fn segment_end(&self, mut node: usize, mut d: u64, mut sibling_idx: u64) -> (usize, u64, u64) {
+        while !Self::is_leaf(d) {
+            let max_children = Self::max_children(d);
+            let Some((idx, &only_child)) = self.branches[node..node + max_children]
+                .iter()
+                .enumerate()
+                .filter(|&(_, &n)| n != Self::NO_CHILD)
+                .exactly_one()
+                .ok()
+            else {
+                break;
+            };
+            node = only_child;
+            d += 1;
+            sibling_idx = idx as u64;
+        }
+        (node, d, sibling_idx)
+    }
+
+    fn is_leaf(s: u64) -> bool {
+        s >= Self::MAX_DEPTH
+    }
+
+    fn max_children(s: u64) -> usize {
+        match (s < 2 * 8, s.is_multiple_of(2)) {
             (true, true) => 8,
             (true, false) => 3,
             (false, true) => 12,
             (false, false) => 2,
         }
     }
+}
 
-    fn branching_impl(&self, node: usize, s: usize) -> Stats {
-        if Self::is_leaf(s) {
-            return Stats {
-                num_nodes: 0,
-                max_num_nodes: 0,
-                num_segments: 1,
-            };
-        }
-        let max_children = Self::max_children(s);
-        let children = self.branches[node..node + max_children]
-            .iter()
-            .filter(|&&n| n != Self::NO_CHILD);
-        let mut stats = children.clone()
-            .map(|&n| self.branching_impl(n, s + 1))
-            .fold(Stats::default(), |s1, s2| s1 + s2);
-        let num_children = children.count();
-        stats.max_num_nodes += max_children;
-        stats.num_nodes += num_children;
-        if num_children != 1 {
-            stats.num_segments += 1;
-        }
-        stats
-    }
-
-    pub fn stats(&self) -> Stats {
-        self.branching_impl(0, 0)
-    }
-
+impl Trie {
     pub fn footprint(&self) -> usize {
         self.branches.len() * size_of::<usize>() + self.states.len() * size_of::<State>()
     }
 }
 
 pub struct TriePtr {
-    state: State,
-    stack: [(usize, u8); 2 * 8 + 2 * 12],
-    size: usize,
+    coset: State,
+    stack: [u64; 2 * 8 + 2 * 12],
+    size: u8,
+    last_d: u8,
 }
 
 impl TriePtr {
     pub fn first(coset: State) -> TriePtr {
+        // This is similar to state inversion, except that the orientation part
+        // is inverted but its position is not swapped.
+        let mut corners = 0;
+        for i in 0..8 {
+            let pos = State::get_block(coset.corners, 5 * i + 2, 3) as u8;
+            let ori = State::get_block(coset.corners, 5 * i, 2);
+            corners |= (i as u64) << (5 * pos + 2);
+            corners |= ((3 - ori) % 3) << (5 * i);
+        }
+        let mut edges = 0;
+        for i in 0..12 {
+            let pos = State::get_block(coset.edges, 5 * i + 1, 4) as u8;
+            let ori = State::get_block(coset.edges, 5 * i, 1);
+            edges |= (i as u64) << (5 * pos + 1);
+            edges |= ori << (5 * i);
+        }
+        let coset = State { corners, edges };
+
         TriePtr {
-            state: coset.invert(),
-            stack: [(0, 0); 2 * 8 + 2 * 12],
+            coset,
+            stack: [0; 2 * 8 + 2 * 12],
             size: 1,
+            last_d: 0,
         }
     }
 
     pub fn next(&mut self, trie: &Trie) -> Option<State> {
         while self.size > 0 {
-            let s = self.size - 1;
-            let (node, idx) = self.stack[s];
-            let pos = idx >> 4; // the position of the corner or edge is given for orientation branches
-            let i = idx & 0x0f;
-            let n = Trie::max_children(s);
+            let top = self.stack[self.size as usize - 1];
+            let node = State::get_block(top, 0, 56) as usize;
+            let i = State::get_block(top, 60, 4) as u8;
+            let branch = trie.branches[node];
+            let d = State::get_block(branch, 58, 6);
+            let n = TrieBuilder::max_children(d);
 
             if (i as usize) < n {
                 // Go to the next branch
-                self.stack[s].1 += 1;
-
-                let j = match (s < 2 * 8, s % 2 == 0) {
-                    (true, true) => State::get_block(self.state.corners, 5 * i + 2, 3) as u8,
+                self.stack[self.size as usize - 1] += 1 << 60;
+                let parent_i = State::get_block(branch, 54, 4) as u8;
+                let j = match (d < 2 * 8, d.is_multiple_of(2)) {
+                    (true, true) => State::get_block(self.coset.corners, 5 * i + 2, 3) as u8,
                     (true, false) => {
-                        (i + State::get_block(self.state.corners, 5 * pos, 2) as u8) % 3
+                        (i + State::get_block(self.coset.corners, 5 * parent_i, 2) as u8) % 3
                     }
-                    (false, true) => State::get_block(self.state.edges, 5 * i + 1, 4) as u8,
+                    (false, true) => State::get_block(self.coset.edges, 5 * i + 1, 4) as u8,
                     (false, false) => {
-                        (i + State::get_block(self.state.edges, 5 * pos, 1) as u8) % 2
+                        (i + State::get_block(self.coset.edges, 5 * parent_i, 1) as u8) % 2
                     }
                 };
 
-                let child_idx = trie.branches[node + j as usize];
-                if child_idx != Trie::NO_CHILD {
-                    if self.size >= self.stack.len() {
-                        return Some(trie.states[child_idx]);
+                // Push the child on the stack if it exists
+                let child_idx = State::get_block(branch, 4 * j, 4);
+                if child_idx != 0 {
+                    let child = trie.branches[node + child_idx as usize];
+                    let leaf = State::get_block(child, 0, 63);
+
+                    // Return the state if the child is a leaf
+                    if child != leaf {
+                        return Some(trie.states[leaf as usize]);
                     }
-                    self.stack[self.size] = (child_idx, i << 4);
+                    self.stack[self.size as usize] = child | ((j as u64) << 56);
                     self.size += 1;
+                    self.last_d = d as u8;
                 }
             } else {
                 // Pop the branch off the stack
                 self.size -= 1;
+                self.last_d = d as u8;
             }
         }
         None
@@ -240,46 +288,49 @@ impl std::cmp::Ord for State {
 
 #[cfg(test)]
 mod tests {
-    use crate::{moves::Move, state::State, trie::Trie, util::Moves};
+    use crate::{moves::Move, state::State, trie::TrieBuilder, util::Moves};
 
     #[test]
     fn trie_normal_ordering() {
-        let mut trie = Trie::new();
+        let mut trie = TrieBuilder::new();
         let mut states: Vec<_> = Moves::to_depth(4).iter().map(|(_, s)| s).collect();
         for &state in states.iter() {
             trie.insert(state);
         }
         states.sort();
+        let trie = trie.build();
         assert_eq!(states, trie.ordered().collect::<Vec<_>>());
     }
 
     #[test]
     fn trie_coset_ordering1() {
-        let mut trie = Trie::new();
+        let mut trie = TrieBuilder::new();
         let state1 = State::SOLVED;
         let state2 = state1.mv(Move::F);
         let state3 = state1.mv(Move::F_);
         trie.insert(state1);
         trie.insert(state2);
         trie.insert(state3);
+        let trie = trie.build();
         let states: Vec<State> = trie.ordered_coset(State::SOLVED.mv(Move::F_)).collect();
         assert_eq!(states, vec![state2, state1, state3]);
     }
 
     #[test]
     fn trie_coset_ordering2() {
-        let mut trie = Trie::new();
+        let mut trie = TrieBuilder::new();
         let state1 = State::SOLVED.mv(Move::D_);
         let state2 = State::SOLVED.mv(Move::L);
         trie.insert(state1);
         trie.insert(state2);
+        let trie = trie.build();
         let states: Vec<State> = trie.ordered_coset(State::SOLVED.mv(Move::F)).collect();
         assert_eq!(states, vec![state1, state2]);
     }
 
     #[test]
     fn trie_coset_ordering3() {
-        let mut trie = Trie::new();
+        let mut trie = TrieBuilder::new();
         let mut states: Vec<_> = Moves::to_depth(4).iter().map(|(_, s)| s).collect();
         for &state in states.iter() {
             trie.insert(state);
@@ -288,6 +339,7 @@ mod tests {
         for state in states.iter_mut() {
             *state = coset.compose(state);
         }
+        let trie = trie.build();
         states.sort();
         assert_eq!(
             states,
