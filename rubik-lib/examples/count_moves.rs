4a24a08aca448352d4de04b3d20ac8fc955c91a4
Antoine Brunner <brunnerant@gmail.com>
2026-06-02 01:33:01 +0200

    Count moves up to symmetries

--- rubik-lib/examples/count_moves.rs
+++ rubik-lib/examples/count_moves.rs
@@ -1,7 +1,10 @@
-use std::env::args;
+use std::{
+    collections::{HashSet, VecDeque},
+    env::args,
+};
 
 use itertools::Itertools;
-use rubik_lib::model::moves::Moves;
+use rubik_lib::model::{moves::Moves, state::State, sym::Symmetries};
 
 fn main() {
     const DEFAULT_DEPTH: u8 = 5;
@@ -14,5 +17,27 @@ fn main() {
         "There are {} unique moves to depth {}",
         Moves::to_depth(depth).unique_by(|&(_, s)| s).count(),
         depth
+    );
+
+    let sym = Symmetries::all();
+    let mut visited = HashSet::new();
+    let mut to_visit = VecDeque::from([(State::ID, 0)]);
+    while let Some((state, d)) = to_visit.pop_front() {
+        visited.insert(state);
+        if d >= depth {
+            continue;
+        }
+        for mv in State::BASIC_MOVES {
+            let next_state = mv * state;
+            let (repr, _) = sym.repr(next_state);
+            if !visited.contains(&repr) {
+                to_visit.push_back((repr, d + 1));
+            }
+        }
+    }
+
+    println!(
+        "This reduces to {} unique moves when factoring out symmetries",
+        visited.len()
     );
 }
