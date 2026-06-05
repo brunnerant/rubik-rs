0bbe8e6d67bee1e78ec5902416465177ca50be2c
Antoine Brunner <brunnerant@gmail.com>
2026-06-06 00:21:46 +0200

    Check the solutions from phase 1

--- rubik-lib/examples/phase1.rs
+++ rubik-lib/examples/phase1.rs
@@ -7,7 +7,7 @@ use itertools::join;
 use rubik_lib::{
     algebra::coord::{CO, Coord, EOLR},
     core::{moves::Move, scramble::scramble, state::State},
-    solve::kociemba::phase1::{self},
+    solve::kociemba::phase1,
 };
 
 fn solve_phase1(
@@ -20,22 +20,30 @@ fn solve_phase1(
     let mut co = CO::from_state(&state).repr();
     let mut next_d = (pruning_table.dist(eolr, co, &coords) + 2) % 3;
     while EOLR::unpack_sym_coord(eolr).0 != 0 || co != 0 {
-        let mv = (0..18).find(|&i| {
-            let next_eolr = coords.eolr_mv.coord_mv(eolr, i, &coords.sym);
-            let next_co = coords.co_mv.coord_mv(co, i);
-            pruning_table.dist(next_eolr, next_co, &coords) == next_d
-        });
-        match mv {
-            Some(i) => {
-                moves.push(Move::BASIC_MOVES[i as usize]);
-                eolr = coords.eolr_mv.coord_mv(eolr, i, &coords.sym);
-                co = coords.co_mv.coord_mv(co, i);
-                next_d = (pruning_table.dist(eolr, co, &coords) + 2) % 3;
-            }
-            None => panic!(),
-        }
+        let (i, next_eolr, next_co) = (0..18)
+            .find_map(|i| {
+                let next_eolr = coords.eolr_mv.coord_mv(eolr, i, &coords.sym);
+                let next_co = coords.co_mv.coord_mv(co, i);
+                (pruning_table.dist(next_eolr, next_co, &coords) == next_d)
+                    .then_some((i, next_eolr, next_co))
+            })
+            .expect("invalid pruning table: no move was found that decreases the distance");
+        moves.push(Move::BASIC_MOVES[i as usize]);
+        eolr = next_eolr;
+        co = next_co;
+        next_d = (next_d + 2) % 3;
     }
     moves
+}
+
+fn check_phase1(mut state: State, moves: &Vec<Move>, coords: &phase1::Coords) {
+    for &mv in moves {
+        state = state * mv;
+    }
+    let (_, j) = EOLR::unpack_sym_coord(coords.eolr_coord.sym_coord(state, &coords.sym));
+    state = coords.sym.conj_inv(state, j);
+    assert_eq!(0, EOLR::from_state(&state).repr());
+    assert_eq!(0, CO::from_state(&state).repr());
 }
 
 fn main() {
@@ -51,6 +59,7 @@ fn main() {
     for _ in 0..100 {
         let (_, state) = scramble(100);
         let moves = solve_phase1(state, &coords, &pruning_table);
+        check_phase1(state, &moves, &coords);
         println!("{}", join(moves, " "));
     }
 }
