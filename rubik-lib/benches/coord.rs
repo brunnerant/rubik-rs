929e1e07945101de863190152bfaee1f8071c05f
Antoine Brunner <brunnerant@gmail.com>
2026-06-08 17:05:13 +0200

    Add benches for coordinates

---
+++ rubik-lib/benches/coord.rs
@@ -0,0 +1,48 @@
+use std::hint::black_box;
+
+use criterion::{Criterion, Throughput, criterion_group};
+use rubik_lib::{
+    algebra::coord::{CO, CP, Coord, EO, LR},
+    core::moves::Moves,
+};
+
+criterion_group!(coord, bench_from_state, bench_to_state);
+
+pub fn bench_from_state(c: &mut Criterion) {
+    fn bench<'a, C: Coord>(c: &mut Criterion, name: &str) {
+        let states: Vec<_> = Moves::to_depth(4).map(|(_, s)| s).collect();
+        let mut g = c.benchmark_group(format!("coord/{name}"));
+        g.throughput(Throughput::Elements(states.len() as u64));
+        g.bench_function("from_state", |b| {
+            b.iter(|| {
+                for s in &states {
+                    black_box(C::from_state(s));
+                }
+            });
+        });
+    }
+
+    bench::<CO>(c, "CO");
+    bench::<EO>(c, "EO");
+    bench::<LR>(c, "LR");
+    bench::<CP>(c, "CP");
+}
+
+pub fn bench_to_state(c: &mut Criterion) {
+    fn bench<'a, C: Coord>(c: &mut Criterion, name: &str) {
+        let mut g = c.benchmark_group(format!("coord/{name}"));
+        g.throughput(Throughput::Elements(C::raw_to_usize(C::RAW_SIZE) as u64));
+        g.bench_function("to_state", |b| {
+            b.iter(|| {
+                for i in C::all_raw_coords() {
+                    black_box(C::from_repr(i).sample_state());
+                }
+            });
+        });
+    }
+
+    bench::<CO>(c, "CO");
+    bench::<EO>(c, "EO");
+    bench::<LR>(c, "LR");
+    bench::<CP>(c, "CP");
+}
--- rubik-lib/benches/main.rs
+++ rubik-lib/benches/main.rs
@@ -2,9 +2,11 @@ use criterion::criterion_main;
 mod product;
 mod state;
 mod trie;
+mod coord;
 
 use product::product;
 use state::state;
 use trie::trie;
+use coord::coord;
 
-criterion_main!(state, trie, product);
+criterion_main!(state, trie, product, coord);
