cda0bcd3cab8e2952f2a8e4cf595991f5b21b34f
Antoine Brunner <brunnerant@gmail.com>
2026-06-09 18:05:02 +0200

    Better display

--- rubik-lib/examples/phase1.rs
+++ rubik-lib/examples/phase1.rs
@@ -1,3 +1,5 @@
+use std::io::Write;
+
 use itertools::join;
 use rubik_lib::{
     algebra::coord::{CO, Coord, EOLR},
@@ -19,6 +21,16 @@ fn check_phase1(mut state: State, moves: &Vec<Move>, coords: &kociemba::Coords) 
     assert_eq!(0, CO::from_state(&state).coord());
 }
 
+fn num_display(n: usize) -> String {
+    if n < 1_000 {
+        format!("{}", n)
+    } else if n < 1_000_000 {
+        format!("{}K", n / 1_000)
+    } else  {
+        format!("{}M", n / 1_000_000)
+    }
+}
+
 fn main() {
     let mut solver = kociemba::Solver::from_folder("data/kociemba").expect("failed to init solver");
     let (mvs, state) = scramble(100);
@@ -26,23 +38,25 @@ fn main() {
     solver.init(&state);
     println!("generating phase 1 move sequences:");
 
-    let mut prev_l = 100;
-    let mut prev_n = 0;
+    let mut l = 100;
+    let mut n = 0;
     loop {
         let Some(mvs) = solver.phase1_step() else {
             break;
         };
 
-        // check_phase1(state, &mvs, &solver.coords);
-        if mvs.len() != prev_l {
-            prev_l = mvs.len();
-            if prev_n > 0 {
-                println!(" done {prev_n}");
-                prev_n = 0;
+        check_phase1(state, &mvs, &solver.coords);
+        if mvs.len() != l {
+            if n > 0 {
+                println!("\rlength {l}: {}", num_display(n));
+                n = 0;
             }
-            print!("length {prev_l}");
+            l = mvs.len();
         }
-        prev_n += 1;
-        // println!("{}", join(mvs, " "));
+        n += 1;
+        if n % 1_000_000 == 0 {
+            print!("\rlength {l}: {}", num_display(n));
+            let _ = std::io::stdout().lock().flush();
+        }
     }
 }
