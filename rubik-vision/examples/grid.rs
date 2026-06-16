424fbbab7760db2e401c1697c82b24e5778f51d5
Antoine Brunner <brunnerant@gmail.com>
2026-06-16 08:53:09 +0200

    Better cv params

--- rubik-vision/examples/grid.rs
+++ rubik-vision/examples/grid.rs
@@ -1,7 +1,14 @@
+use std::f32::consts::TAU;
 use std::ffi::OsStr;
 
 use opencv::core::Vector;
 use opencv::imgcodecs::{IMREAD_COLOR, imread, imwrite};
+use plotters::backend::SVGBackend;
+use plotters::chart::{ChartBuilder, LabelAreaPosition};
+use plotters::drawing::IntoDrawingArea;
+use plotters::element::Circle;
+use plotters::series::PointSeries;
+use plotters::style::{BLUE, WHITE};
 use rubik_vision::grid::lines;
 use workspace_root::get_workspace_root;
 
@@ -16,8 +23,25 @@ fn main() {
             continue;
         }
         let out = out_folder.join(img.file_name().unwrap());
+        let out_graph = out_folder.join(img.file_name().unwrap()).with_extension("svg");
         let mut img = imread(img.to_str().unwrap(), IMREAD_COLOR).unwrap();
         let lines = lines(&mut img).unwrap();
         imwrite(out.to_str().unwrap(), &img, &Vector::new()).unwrap();
+
+        let root_area = SVGBackend::new(&out_graph, (600, 400)).into_drawing_area();
+        root_area.fill(&WHITE).unwrap();
+        let mut ctx = ChartBuilder::on(&root_area)
+            .set_label_area_size(LabelAreaPosition::Left, 40)
+            .set_label_area_size(LabelAreaPosition::Bottom, 40)
+            .caption("Scatter Plot Demo", ("sans-serif", 40))
+            .build_cartesian_2d(0.0..TAU, 0.0f32..500.0).unwrap();
+        ctx.configure_mesh().draw().unwrap();
+        ctx.draw_series(
+            PointSeries::<_, _, Circle<_, _>, _>::new(
+                lines.iter().map(|l| (l.theta, l.rho)),
+                5,
+                &BLUE,
+            )
+        ).unwrap();
     }
 }
--- rubik-vision/src/grid.rs
+++ rubik-vision/src/grid.rs
@@ -2,25 +2,25 @@ use std::f32::consts::{PI, TAU};
 
 use opencv::{
     core::{BORDER_DEFAULT, CV_FP16, Mat, MatTraitConst, Point, Scalar, Vec4f, VecN, Vector},
-    imgproc::{LINE_AA, canny, dilate, hough_lines_p, line},
+    imgproc::{LINE_AA, canny, dilate, erode, hough_lines_p, line},
 };
 
 #[derive(Clone, Copy, Debug)]
 pub struct Line {
-    pub theta: f32,
     pub rho: f32,
+    pub theta: f32,
 }
 
 impl Line {
     pub fn from_segment(a: (f32, f32), b: (f32, f32)) -> Self {
         let (dx, dy) = (b.0 - a.0, b.1 - a.1);
-        let mut theta = (dx.atan2(dy) + TAU) % TAU;
-        let mut rho = a.1 * theta.cos() - a.0 * theta.sin();
+        let mut theta = (dx.atan2(-dy) + TAU) % TAU;
+        let mut rho = a.0 * theta.cos() + a.1 * theta.sin();
         if rho < 0.0 {
             rho = -rho;
             theta = (theta + PI) % TAU;
         }
-        Self { theta, rho }
+        Self { rho, theta: (theta + 0.5 * PI) % TAU }
     }
 }
 
@@ -29,26 +29,36 @@ pub fn lines(img: &mut Mat) -> opencv::Result<Vec<Line>> {
         Mat::new_rows_cols_with_default(img.rows(), img.cols(), img.typ(), Scalar::default())?;
     let mut edges_dil: Mat =
         Mat::new_rows_cols_with_default(img.rows(), img.cols(), img.typ(), Scalar::default())?;
-    let kernel = Mat::new_rows_cols_with_default(3, 3, CV_FP16, 1.0.into()).unwrap();
-    canny(img, &mut edges, 100.0, 200.0, 3, false).unwrap();
+        canny(img, &mut edges, 150.0, 250.0, 3, false).unwrap();
+    let kernel = Mat::new_rows_cols_with_default(3,3, CV_FP16, 1.0.into()).unwrap();
     dilate(
         &mut edges,
         &mut edges_dil,
         &kernel,
         Point::new(-1, -1),
-        1,
+        2,
         BORDER_DEFAULT,
         VecN::new(0.0, 0.0, 0.0, 0.0),
     )?;
+    erode(
+        &mut edges_dil,
+        &mut edges,
+        &kernel,
+        Point::new(-1, -1),
+        1,
+        BORDER_DEFAULT,
+        VecN::new(0.0, 0.0, 0.0, 0.0),
+    )
+    .unwrap();
     let mut lines = Vector::<Vec4f>::new();
     hough_lines_p(
-        &edges_dil,
+        &edges,
         &mut lines,
         1.0,
         1.0f64.to_radians(),
-        50,
-        100.0,
-        25.0,
+        100,
+        30.0,
+        10.0,
     )?;
     let mut out_lines = Vec::with_capacity(lines.len());
     for l in lines {
